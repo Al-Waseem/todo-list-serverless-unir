c046645c54a006e2098134697cef8c6fb2f1bdde
Rubén Galeano <rubengaleanounir@gmail.com>
2021-05-21 18:15:16 +0000

    Added translate function

--- todos/getTranslate.py
+++ todos/getTranslate.py
@@ -1,9 +1,26 @@
 import os
 import json
 
-from todos import decimalencoder
 import boto3
+from todos import decimalencoder
+
 dynamodb = boto3.resource('dynamodb')
+translate = boto3.client('translate')
+comprehend = boto3.client('comprehend')
+
+def detect_language_task(task):
+    response = comprehend.detect_dominant_language(Text='string')
+    
+    return response
+
+def translate_task(task, source, target):
+
+    response = translate.translate_text(
+    Text=task,
+    SourceLanguageCode = source,
+    TargetLanguageCode = target)
+    
+    return response
 
 
 def get(event, context):
@@ -15,7 +32,18 @@ def get(event, context):
             'id': event['pathParameters']['id']
         }
     )
+    
+    target = event['pathParameters']['lang']
 
+    task = result['Item']['text']
+
+    source_result = detect_language_task(task)
+    
+    source = source_result['Languages'][0]['LanguageCode']
+    
+    task_translated = translate_task(task, source, target)
+    result['Item']['text'] = task_translated['TranslatedText']
+    
     # create a response
     response = {
         "statusCode": 200,
